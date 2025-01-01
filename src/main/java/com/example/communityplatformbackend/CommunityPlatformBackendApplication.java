5ac275b0682f0c2c93b5f6a8f732d4997d6dea16
Jong1 <152011487+abgf0103@users.noreply.github.com>
2025-01-01 22:44:05 +0900

    Set up project

---
+++ src/main/java/com/example/communityplatformbackend/CommunityPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.communityplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityPlatformBackendApplication.class, args);
+    }
+
+}
